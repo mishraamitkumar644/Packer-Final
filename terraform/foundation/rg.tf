6da6d3908f1becc5be398494363f2878a80e325f
mishraamitkumar644 <69366648+mishraamitkumar644@users.noreply.github.com>
2026-05-22 08:04:17 +0000

    Update rg.tf

--- terraform/foundation/rg.tf
+++ terraform/foundation/rg.tf
@@ -1,12 +1,12 @@
 # If RG already exists
-data "azurerm_resource_group" "existing" {
-  count = var.resource_group_name ? 0 : 1
+data "azurerm_resource_group" "rg" {
+  count = var.create_rg ? 0 : 1
 
   name = var.resource_group_name
 }
   
 resource "azurerm_resource_group" "rg" {
- count = var.resource_group_name ? 1 : 0
+ count = var.create_rg ? 1 : 0
   name     = var.resource_group_name
   location = var.location
 }
