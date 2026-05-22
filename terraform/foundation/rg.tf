d2200f720f0909d732b2639da6a5daefe9691cea
mishraamitkumar644 <69366648+mishraamitkumar644@users.noreply.github.com>
2026-05-22 08:48:12 +0000

    Remove existing resource group data block
    Removed data block for existing resource group check.

--- terraform/foundation/rg.tf
+++ terraform/foundation/rg.tf
@@ -4,7 +4,4 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
-# If RG already exists
-data "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
-}
+
