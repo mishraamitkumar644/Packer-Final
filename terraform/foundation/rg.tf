a5a4304391ca8d516575881ab498fb155a8a9ad3
mishraamitkumar644 <69366648+mishraamitkumar644@users.noreply.github.com>
2026-05-22 07:51:35 +0000

    Implement existing resource group data source
    Add data source for existing resource group check.

--- terraform/foundation/rg.tf
+++ terraform/foundation/rg.tf
@@ -1,5 +1,12 @@
-resource "azurerm_resource_group" "rg" {
+# If RG already exists
+data "azurerm_resource_group" "existing" {
+  count = var.create_rg ? 0 : 1
 
+  name = var.resource_group_name
+}
+  
+resource "azurerm_resource_group" "rg" {
+ count = var.create_rg ? 1 : 0
   name     = var.resource_group_name
   location = var.location
 }
