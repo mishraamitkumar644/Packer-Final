f65b99e031dd5fab53b24acedb4eebf13543194b
mishraamitkumar644 <69366648+mishraamitkumar644@users.noreply.github.com>
2026-05-22 08:51:55 +0000

    Update sig.tf

--- terraform/foundation/sig.tf
+++ terraform/foundation/sig.tf
@@ -2,5 +2,5 @@ resource "azurerm_shared_image_gallery" "sig" {
 
   name                = var.gallery_name
   resource_group_name = local.rg_name
-  location            = local.rg_location
+  location            = var.location
 }
