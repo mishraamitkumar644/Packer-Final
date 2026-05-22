61681a81abfbee509c99a4f3bb9a16977ad5f159
mishraamitkumar644 <69366648+mishraamitkumar644@users.noreply.github.com>
2026-05-22 08:09:38 +0000

    Update resource group reference to use index

--- terraform/foundation/sig.tf
+++ terraform/foundation/sig.tf
@@ -1,6 +1,6 @@
 resource "azurerm_shared_image_gallery" "sig" {
 
   name                = var.gallery_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg[0].name
+  location            = azurerm_resource_group.rg[0].location
 }
