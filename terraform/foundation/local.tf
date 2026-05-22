0eadb77a3603856a3f43eabc0cde4ec35eacfd05
mishraamitkumar644 <69366648+mishraamitkumar644@users.noreply.github.com>
2026-05-22 09:51:23 +0000

    Update local.tf

--- terraform/foundation/local.tf
+++ terraform/foundation/local.tf
@@ -1,3 +1,3 @@
 locals {
-  rg_name = var.create_rg ? azurerm_resource_group.rg[0].name : var.resource_group_name
+  rg_name = var.create_rg ? azurerm_resource_group.rg.name : var.resource_group_name
 }
