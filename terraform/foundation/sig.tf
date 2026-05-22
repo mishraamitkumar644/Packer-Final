7bd59e51757fc4cf77d0ab9b79549b83af48a762
mishraamitkumar644 <69366648+mishraamitkumar644@users.noreply.github.com>
2026-05-22 07:31:22 +0000

    Refactor resource group and location variables

--- terraform/foundation/sig.tf
+++ terraform/foundation/sig.tf
@@ -1,6 +1,8 @@
 resource "azurerm_shared_image_gallery" "sig" {
 
   name                = var.gallery_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name     = var.resource_group_name
+  location = var.location
+  #resource_group_name = azurerm_resource_group.rg.name
+  #location            = azurerm_resource_group.rg.location
 }
